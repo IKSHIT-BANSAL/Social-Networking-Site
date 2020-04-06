b58e031f7624b73fa3131ca28081a34cfdf03f02
ikshit bansal <ikshitbansal1998@gmail.com>
2020-04-06 13:11:51 +0530

    Post controller is added

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express=require('express');
 const router=express.Router();
 
 const homeController=require('../controllers/home_controller');
-console.log('Routes is running');
+// console.log('Routes is running');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users.js'));
+router.use('/postUsers',require('./posts.js'));
 
 
 module.exports=router;
