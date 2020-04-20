1e6a6f68ce582e63f84dca86c2ef4927f687d3cc
ikshit bansal <ikshitbansal1998@gmail.com>
2020-04-20 13:53:34 +0530

    Post displayed with user name

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,22 @@
+const Post=require('../models/post');
+
 module.exports.home=function(req,res){
 
     console.log(req.cookies);
     res.cookie('user_id',25);
-    return res.render('home',{
-        title:"Home",
+
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title:"Home",
+    //         posts:posts
+    //     });
+    // })
+
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Home",
+            posts:posts
+        });
     });
 
     // return res.end('<h1> At Home Controller </h1>');
