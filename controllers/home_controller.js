c7bbf7cc5aca1f2916f4194e695525e5b7ce13be
ikshit bansal <ikshitbansal1998@gmail.com>
2020-04-22 13:58:15 +0530

    comments are shown with user name who commented

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -11,7 +11,15 @@ module.exports.home=function(req,res){
     //         posts:posts
     //     });
     // })
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    .exec(function(err,posts){
         return res.render('home',{
             title:"Home",
             posts:posts
