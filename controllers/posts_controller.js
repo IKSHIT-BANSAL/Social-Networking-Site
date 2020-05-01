fe4bcf12e0faa55e412f9fda0caa037fe4b5cb0c
IKSHT-BANSAL <ikshitbansal1998@gmail.com>
2020-05-01 13:58:21 +0530

    Ajax added for deleting a post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -36,6 +36,15 @@ module.exports.destroy=async function(req,res){
             post.remove();      //remove that post from database
         
             await Comment.deleteMany({post:req.params.id});
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        post_id:req.params.id
+                    },
+                    message:'Post deleted'
+                })
+            }
             
             req.flash('success','Post and associated comments are deleted');
 
