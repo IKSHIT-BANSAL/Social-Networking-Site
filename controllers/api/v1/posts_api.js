b21e1ac83f9d55435cb93b2e0e5834cf3c4eb6ef
IKSHT-BANSAL <ikshitbansal1998@gmail.com>
2020-05-11 01:01:22 +0530

    Post deleted with API without user authentication

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,52 @@
-module.exports.index=function(req,res){
+const Post=require('../../../models/post');
+const Comment=require('../../../models/comment');
+
+module.exports.index=async function(req,res){
+
+    let posts=await Post.find({})
+    .sort('-createdAt')
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    });
+
     return res.json(200,{
         message:'List of Post',
-        posts:[]
+        posts:posts
     })
 }
+
+module.exports.destroy=async function(req,res){
+    try {
+        let post=await Post.findById(req.params.id);
+      
+            post.remove();      //remove that post from database
+        
+           await Comment.deleteMany({post:req.params.id});
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        post_id:req.params.id
+                    },
+                    message:'Post deleted',
+                    
+                    
+                })
+            }        
+
+            return res.json(200,{
+                message:'Post and associated comments deleted !!'
+            }) 
+    }catch (err) {
+        if(err){
+            console.log('*****',err);
+            return res.json(500,{
+                message:'Internal server error'
+            })
+        }
+    }
+};
