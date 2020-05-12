5aedc31fdb8cafc7fbf1028648f5bf0b4a7b5c90
IKSHT-BANSAL <ikshitbansal1998@gmail.com>
2020-05-12 14:42:35 +0530

    JSON token generated

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -1,12 +1,12 @@
 const passport=require('passport');
 const JWTStrategy=require('passport-jwt').Strategy;
-const ExtractJWT=require('passport-jwt').ExtractJwt;
+const ExtractJWT=require('passport-jwt').ExtractJwt;    //To extract header from JWT
 
 const User=require('../models/user');
 
 let opts={
-    jwtFromRequest:ExtractJWT.fromAuthHeaderAsBearerToken,
-    secretOrKey:'codeial' 
+    jwtFromRequest:ExtractJWT.fromAuthHeaderAsBearerToken,      //fetching a bearer key in header
+    secretOrKey:'codeial'       //a token will be generated using this key
 }
 
 passport.use(new JWTStrategy(opts,function(jwtPayLoad,done){
