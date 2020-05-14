4bc7daf5c22eb460ecd7f5d67ae3ae76fe8e436d
IKSHT-BANSAL <ikshitbansal1998@gmail.com>
2020-05-14 13:17:13 +0530

    comments added to passport google auth2-strategy file

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -9,9 +9,12 @@ passport.use(new googleStrategy({
     clientSecret:"ujBbRXIw_qrGoRj2mTtOpolc",
     callbackURL:"http://localhost:8000/users/auth/google/callback"
     
+    //accesstoken is like jwt token we genrated
+    //refreshtoken if our accesstoken expires then we use it to get a new token
+    //profile is information of user
 },function(accessToken,refreshToken,profile,done){
     //find a user 
-    User.findOne({email:profile.emails[0].value}).exec(function(err,user){
+    User.findOne({email:profile.emails[0].value}).exec(function(err,user){      //profile.email[] is array so we fetch its 1 value
         if(err){
             console.log('error in google strategy passport',err);
             return;
@@ -26,10 +29,10 @@ passport.use(new googleStrategy({
             User.create({
                 name:profile.displayName,
                 email:profile.emails[0].value,
-                password:crypto.randomBytes(20).toString('hex')
+                password:crypto.randomBytes(20).toString('hex')   //this how crypto works fetching 20 bytes and string in hexadecimal
             },function(err,user){
                 if(err){
-                    console.log('error in google strategy passport',err);
+                    console.log('error in creating user google strategy passport',err);   
                     return;
                 }
                 return done(null,user);
